899b5c3bac9ab3bdcfae62c5d52f1668f6585d0e
aadvika <66198185+aadvika@users.noreply.github.com>
2020-05-31 13:42:38 +0530

    Create CheckFibonacci.java
    To check if a number is fibonacci or not

---
+++ CheckFibonacci.java
@@ -0,0 +1,25 @@
+//To check if a number is fibonacci or not
+
+public class CheckFibonacci {
+
+    public static void main(String[] args) {
+        int n = 35;
+        boolean isFib = checkFibonacci(n);
+        System.out.println("isFib = " + isFib);
+    }
+
+    //number is fibonacci if 5*n*n+4 or 5*n*n-4 is perfect square
+    static boolean checkFibonacci(int n) {
+        if(isPerfectSquare(5*n*n + 4) || isPerfectSquare(5*n*n - 4))
+            return true;
+        else
+            return false;
+    }
+
+    //function to check if a number is perfect square
+    static boolean isPerfectSquare(int n) {
+        int x = (int) Math.sqrt(n);
+        if(x*x == n) return true;
+        else return false;
+    }
+}
